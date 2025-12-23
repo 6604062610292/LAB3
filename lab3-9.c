0b3361fbdd1a6361c3ad10ea13e59377bdcc41f1
ชานนท์ สายวิสัย <s6604062610292@email.kmutnb.ac.th>
2025-12-23 22:59:24 +0700

    Add files via upload

---
+++ lab3-9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (){
+	int month;
+	scanf("%d",&month);
+	switch(month){
+		case 1 :
+		case 2 :
+		case 12 : printf("Winter"); break;
+		case 3 :
+		case 4 :
+		case 5 : printf("Spring"); break;
+		case 6 :
+		case 7 :
+		case 8 : printf("Summer"); break;
+		case 9 :
+		case 10 :
+		case 11 : printf("Autumn"); break;
+		default : printf("No season");
+	}
+}
+
